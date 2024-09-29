335806ecd66c7462dd6b9cc95a5b44c40444a105
Joyce He <joyce@Joyces-MacBook-Pro.local>
2024-09-28 17:08:09 -0700

    trying new emailer php

--- dist/mail_handler.php
+++ dist/mail_handler.php
@@ -9,29 +9,29 @@ $subject = "New form submission";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@demosite.com";
 
-if($email != NULL) {
-    mail($to, $subject, $txt, $headers);
-    echo "Hi " . $name . "! Thank you for checking out my art and reaching out. I will follow up with you shortly.";
-}
-?>
+require "vendor/autoload.php";
 
-/*
-if(isset($_POST['submit'])){
-    $to = "byjiarong@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $name = $_POST['name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = . $name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Hi " . $name . "! Thank you for checking out my art and reaching out. I will follow up with you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
-    }
-?> 
-*/
+$mail = new PHPMailer(true);
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "byjiarong@gmail.com";
+$mail->Password = "Ceramics123!";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("byjiarong@gmail.com", "Joyce");
+
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
+
+echo "email sent"
